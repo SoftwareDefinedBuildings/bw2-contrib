8f26313e0dc0fcd28959837ef43a64c8e171332d
Gabe Fierro <gtfierro225@gmail.com>
2016-07-21 14:19:13 -0700

    update weather underground driver

--- driver/weatherunderground/main.go
+++ driver/weatherunderground/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	msgs "github.com/gtfierro/giles2/plugins/bosswave"
 	"github.com/immesys/spawnpoint/spawnable"
 	"github.com/satori/go.uuid"
 	bw2 "gopkg.in/immesys/bw2bind.v5"
@@ -15,20 +14,32 @@ func init() {
 	NAMESPACE_UUID = uuid.FromStringOrNil("d8b61708-2797-11e6-836b-0cc47a0f7eea")
 }
 
+type TimeseriesReading struct {
+	UUID  string
+	Time  int64
+	Value float64
+}
+
+func (msg TimeseriesReading) ToMsgPackBW() (po bw2.PayloadObject) {
+	po, _ = bw2.CreateMsgPackPayloadObject(bw2.FromDotForm("2.0.9.1"), msg)
+	return
+}
+
 func main() {
 	bw := bw2.ConnectOrExit("")
 
 	params := spawnable.GetParamsOrExit()
 	apikey := params.MustString("API_KEY")
 	city := params.MustString("city")
-	baseuri := params.MustString("path")
+	baseuri := params.MustString("svc_base_uri")
 	read_rate := params.MustString("read_rate")
 
 	bw.OverrideAutoChainTo(true)
 	bw.SetEntityFromEnvironOrExit()
 	svc := bw.RegisterService(baseuri, "s.weatherunderground")
 	iface := svc.RegisterInterface(city, "i.weather")
-	iface.SetMetadata("archive", "true")
+
+	params.MergeMetadata(bw)
 
 	fmt.Println(iface.FullURI())
 	fmt.Println(iface.SignalURI("fahrenheit"))
@@ -38,39 +49,17 @@ func main() {
 	temp_c_uuid := uuid.NewV3(NAMESPACE_UUID, city+"celsius").String()
 	relative_humidity_uuid := uuid.NewV3(NAMESPACE_UUID, city+"relative_humidity").String()
 
-	bw.PublishOrExit(&bw2.PublishParams{
-		URI:            iface.SignalURI("fahrenheit") + "/!meta/archive",
-		PayloadObjects: []bw2.PayloadObject{bw2.CreateStringPayloadObject(iface.SignalURI("fahrenheit"))},
-		Persist:        true,
-	})
-
-	bw.PublishOrExit(&bw2.PublishParams{
-		URI:            iface.SignalURI("celsius") + "/!meta/archive",
-		PayloadObjects: []bw2.PayloadObject{bw2.CreateStringPayloadObject(iface.SignalURI("celsius"))},
-		Persist:        true,
-	})
-
-	bw.PublishOrExit(&bw2.PublishParams{
-		URI:            iface.SignalURI("relative_humidity") + "/!meta/archive",
-		PayloadObjects: []bw2.PayloadObject{bw2.CreateStringPayloadObject(iface.SignalURI("relative_humidity"))},
-		Persist:        true,
-	})
-
 	src := NewWeatherUndergroundSource(apikey, city, read_rate)
 	data := src.Start()
 	for point := range data {
 		fmt.Println(point)
-		temp_f := msgs.Point{Time: uint64(time.Now().Unix()), Value: point.F}
-		temp_f_msg := msgs.Timeseries{UUID: temp_f_uuid, Data: []msgs.Point{temp_f}}
-		iface.PublishSignal("fahrenheit", temp_f_msg.ToMsgPackBW())
+		temp_f := TimeseriesReading{UUID: temp_f_uuid, Time: time.Now().Unix(), Value: point.F}
+		iface.PublishSignal("fahrenheit", temp_f.ToMsgPackBW())
 
-		temp_c := msgs.Point{Time: uint64(time.Now().Unix()), Value: point.C}
-		temp_c_msg := msgs.Timeseries{UUID: temp_c_uuid, Data: []msgs.Point{temp_c}}
-		iface.PublishSignal("celsius", temp_c_msg.ToMsgPackBW())
+		temp_c := TimeseriesReading{UUID: temp_c_uuid, Time: time.Now().Unix(), Value: point.C}
+		iface.PublishSignal("celsius", temp_c.ToMsgPackBW())
 
-		relative_humidity := msgs.Point{Time: uint64(time.Now().Unix()), Value: point.RH}
-		relative_humidity_msg := msgs.Timeseries{UUID: relative_humidity_uuid, Data: []msgs.Point{relative_humidity}}
-		iface.PublishSignal("relative_humidity", relative_humidity_msg.ToMsgPackBW())
+		rh := TimeseriesReading{UUID: relative_humidity_uuid, Time: time.Now().Unix(), Value: point.RH}
+		iface.PublishSignal("relative_humidity", rh.ToMsgPackBW())
 	}
-
 }
