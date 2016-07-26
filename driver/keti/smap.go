55a1dafb5cfa287bda39de9ea9d769b5e47ba4a5
Gabe Fierro <gtfierro225@gmail.com>
2016-07-26 14:10:01 -0700

    need to marshal json differently for smap archiver

--- driver/keti/smap.go
+++ driver/keti/smap.go
@@ -10,48 +10,42 @@ import (
 	"sync"
 )
 
+type Wrapper struct {
+	Path    string
+	Reading SmapReading
+}
+
 type SmapReading struct {
 	UUID     string
 	Readings [][]json.Number
 }
 
-func (msg TimeseriesReading) ToSmapReading(path string) ([]byte, error) {
+func (rdg *SmapReading) AddReading(time int64, value float64) {
+	floatString := strconv.FormatFloat(value, 'f', -1, 64)
+	timeString := strconv.FormatInt(time, 10)
+	rdg.Readings = append(rdg.Readings, []json.Number{json.Number(timeString), json.Number(floatString)})
+}
+
+func (msg TimeseriesReading) toSmapReading() *SmapReading {
 	floatString := strconv.FormatFloat(msg.Value, 'f', -1, 64)
 	timeString := strconv.FormatInt(msg.Time, 10)
 	rdg := &SmapReading{
 		UUID:     msg.UUID,
 		Readings: [][]json.Number{[]json.Number{json.Number(timeString), json.Number(floatString)}},
 	}
-	return json.Marshal(map[string]*SmapReading{
-		path: rdg,
-	})
-}
-
-func (msg TimeseriesReading) SendToSmap(msgPath, uri string) error {
-	mybytes, err := msg.ToSmapReading(msgPath)
-	if err != nil {
-		return err
-	}
-	resp, err := http.Post(uri, "application/json", bytes.NewBuffer(mybytes))
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("Got status code %d\n", resp.StatusCode)
-	}
-	return nil
+	return rdg
 }
 
 type BufferedSender struct {
 	uri      string
-	tosend   []byte
+	tosend   map[string]*SmapReading
 	max, num int
 	sync.Mutex
 }
 
 func NewBufferedSender(uri string, max int) *BufferedSender {
 	return &BufferedSender{
-		tosend: []byte{},
+		tosend: make(map[string]*SmapReading),
 		uri:    uri,
 		max:    max,
 		num:    0,
@@ -59,18 +53,22 @@ func NewBufferedSender(uri string, max int) *BufferedSender {
 }
 
 func (buf *BufferedSender) Send(path string, msg TimeseriesReading) error {
-	b, err := msg.ToSmapReading(path)
+	buf.Lock()
+	defer buf.Unlock()
+	if _, found := buf.tosend[path]; found {
+		buf.tosend[path].AddReading(msg.Time, msg.Value)
+	} else {
+		buf.tosend[path] = msg.toSmapReading()
+	}
+	buf.num += 1
+	if buf.num < buf.max {
+		return nil
+	}
+	// here, send
+	sendme, err := json.Marshal(buf.tosend)
 	if err != nil {
 		return err
 	}
-	buf.Lock()
-	defer buf.Unlock()
-	buf.num += 1
-	if buf.num < buf.max {
-		buf.tosend = append(buf.tosend, b...)
-		return nil
-	}
-	sendme := buf.tosend
 	resp, err := http.Post(buf.uri, "application/json", bytes.NewBuffer(sendme))
 	if err != nil {
 		return err
@@ -80,7 +78,7 @@ func (buf *BufferedSender) Send(path string, msg TimeseriesReading) error {
 		reason, _ := ioutil.ReadAll(resp.Body)
 		return fmt.Errorf("Got status code %d: %s\n", resp.StatusCode, reason)
 	}
-	buf.tosend = []byte{}
+	buf.tosend = make(map[string]*SmapReading)
 	buf.num = 0
 	return nil
 }
