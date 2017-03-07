ee6e7db4eea1f33c169274fd72d05e8caef28d0f
Peter Xu <peterxu30@users.noreply.github.com>
2017-03-07 12:04:02 -0800

    working vplug

--- driver/vplug/vplug.go
+++ driver/vplug/vplug.go
@@ -1,30 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 type Vplug struct {
 	status bool
-	rate time.Duration
-	data chan PlugStats
 }
 
-type PlugStats struct {
-	info bool
-}
-
-func NewVplug(rate string) *Vplug {
-	dur, err := time.ParseDuration(rate)
-	if err != nil {
-		panic(err)
-	}
-
+func NewVplug() *Vplug {
 	return &Vplug{
-		stat
-		rate: dur,
-		data: make(chan PlugStats),
+		status: false,
 	}
 }
 
@@ -32,8 +14,6 @@ func (v *Vplug) ActuatePlug(status bool) {
 	v.status = status
 }
 
-func (v *Vplug) GetStatus() PlugStats {
-	return PlugStats {
-		info: v.status,
-	}
+func (v *Vplug) GetStatus() bool {
+	return v.status
 }
