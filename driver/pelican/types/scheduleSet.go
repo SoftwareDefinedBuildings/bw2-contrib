83cf752091acd417ecbba15d032b1f88c20552f1
john-b-yang <byjohnyang@gmail.com>
2019-02-06 13:40:03 -0800

    segmented setting functionality into delete and construct stages.

--- driver/pelican/types/scheduleSet.go
+++ driver/pelican/types/scheduleSet.go
@@ -7,11 +7,26 @@ import (
 )
 
 func (pel *Pelican) SetSchedule(newSchedule *ThermostatSchedule) error {
-	// Iterate and create schedules day by day
 	for day, blocks := range newSchedule.DaySchedules {
-		// Build HTTP Request's value by day
-		var value string
-		for _, block := range blocks {
+
+		// Delete Day's Existing Schedule
+		respDelete, _, errsDelete := pel.scheduleReq.Get(pel.target).
+			Param("username", pel.username).
+			Param("password", pel.password).
+			Param("request", "set").
+			Param("object", "thermostatSchedule").
+			Param("selection", fmt.Sprintf("name:%s;dayOfWeek:%s;", pel.Name, day)).
+			Param("value", "deleteAll").
+			End()
+
+		if errsDelete != nil {
+			return fmt.Errorf("Error deleting thermostat schedule settings on day %v: %v\n", day, errsDelete)
+		}
+
+		defer respDelete.Body.Close()
+
+		for index, block := range blocks {
+			var value string = ""
 			value += fmt.Sprintf("coolSetting:%.0f;", block.CoolSetting)
 			value += fmt.Sprintf("heatSetting:%.0f", block.HeatSetting)
 			value += fmt.Sprintf("system:%s", block.System)
@@ -20,39 +35,27 @@ func (pel *Pelican) SetSchedule(newSchedule *ThermostatSchedule) error {
 			if timeErr != nil {
 				return fmt.Errorf("Error converting time string %v to RRule format: %v\n", block.Time, timeErr)
 			}
-			// TODO(john-b-yang) Might be start (24 hr), not set time?
+
 			hour := time.OrigOptions.Dtstart.Hour()
 			minute := time.OrigOptions.Dtstart.Minute()
-			meridiem := "AM"
-			if hour > 12 {
-				hour -= 12
-				meridiem = "PM"
+			value += fmt.Sprintf("startTime:%s;", fmt.Sprintf("%v:%v", hour, minute))
+
+			// Set (Day, Time)'s Schedule
+			respSet, _, errsSet := pel.scheduleReq.Get(pel.target).
+				Param("username", pel.username).
+				Param("password", pel.password).
+				Param("request", "set").
+				Param("object", "thermostatSchedule").
+				Param("selection", fmt.Sprintf("name:%s;dayOfWeek:%s;setTime:%v;", pel.Name, day, index)).
+				Param("value", value).
+				End()
+
+			if errsSet != nil {
+				return fmt.Errorf("Error setting schedule for thermostat on day %v at index %v: %v", day, index, errsSet)
 			}
-			value += fmt.Sprintf("setTime:%s;", fmt.Sprintf("%v:%v:%v", hour, minute, meridiem))
 
-			// Remove existing schedule
-			// TODO(john-b-yang) Should this be a separate go request before creating the new schedule?
-			value += fmt.Sprintf("deleteAll")
+			defer respSet.Body.Close()
 		}
-
-		// Constructing HTTP Request
-		// TODO(john-b-yang) Replace w/ "schedReq"
-		resp, _, errs := pel.req.Get(pel.target).
-			Param("username", pel.username).
-			Param("password", pel.password).
-			Param("request", "set").
-			Param("object", "thermostatSchedule").
-			Param("selection", fmt.Sprintf("name:%s", pel.Name)).
-			Param("dayOfWeek", day). // TODO(john-b-yang) ":", not "="
-			Param("value", value).
-			End()
-
-		if errs != nil {
-			return fmt.Errorf("Error modifying thermostat schedule settings: %v\n", errs)
-		}
-
-		defer resp.Body.Close()
-		fmt.Printf("%v\n", resp.Request.URL)
 	}
 
 	return nil
